753d3a8a01d1addb9553370effcc77fd4df20b40
rajeshrajee37 <rajeshrajee37@gmail.com>
2024-03-05 06:38:24 +0530

    add

--- obj/Debug/net8.0/MyDotNetApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyDotNetApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyDotNetApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fa1ddc1fe7da64455992ae87d0bc1b23e15ae87")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e833bff889621396e0c3197892a89ae767170b69")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyDotNetApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyDotNetApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
